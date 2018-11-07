b47cd2722ee423ca3a2388d6e72ec1cd8139790e
Casper Søndergaard <casp4807@hotmail.com>
2018-11-07 11:50:57 +0100

    Electron added

---
+++ Controllers/MatchmakingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LOS.Controllers
+{
+    public class MatchmakingController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+
+            return View();
+        }
+
+        public IActionResult fivevfive()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void SearchForMatch()
+        {
+
+
+
+        }
+
+    }
+}
