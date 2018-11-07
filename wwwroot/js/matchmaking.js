d4a92b8901a306f0c2e07043df155bc524a3a9ff
Casper Søndergaard <casp4807@hotmail.com>
2018-11-07 13:20:41 +0100

    Matchmaking page added

---
+++ wwwroot/js/matchmaking.js
@@ -0,0 +1,38 @@
+﻿function oneVsoneQueue(summonerId, region) {
+    $(".mm_gametype").css({ "display": "none" });
+    $(".searching_wrap").toggle();
+    startTimer();
+}
+
+
+function startTimer() {
+    var elapsed_seconds = 0;
+    setInterval(function () {
+        elapsed_seconds = elapsed_seconds + 1;
+        $(".searching_circle>p").text(get_elapsed_time_string(elapsed_seconds));
+    }, 1000);
+}
+
+function get_elapsed_time_string(total_seconds) {
+    function pretty_time_string(num) {
+        return (num < 10 ? "0" : "") + num;
+    }
+
+    var hours = Math.floor(total_seconds / 3600);
+    total_seconds = total_seconds % 3600;
+
+    var minutes = Math.floor(total_seconds / 60);
+    total_seconds = total_seconds % 60;
+
+    var seconds = Math.floor(total_seconds);
+
+    // Pad the minutes and seconds with leading zeros, if required
+    hours = pretty_time_string(hours);
+    minutes = pretty_time_string(minutes);
+    seconds = pretty_time_string(seconds);
+
+    // Compose the string for display
+    var currentTimeString =  minutes + ":" + seconds;
+
+    return currentTimeString;
+}
